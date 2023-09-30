a7c1ac031c82e978c9465e235b038b7cfce17879
Ashley Dennis <adenn1852@gmail.com>
2023-09-30 14:31:54 -0400

    link to table added

--- city-hackathon-fe/src/components/ElementNav.vue
+++ city-hackathon-fe/src/components/ElementNav.vue
@@ -33,7 +33,7 @@
                 />
               </svg>
             </span>
-            <span>Link 1</span>
+            <span>Home</span>
           </a>
         </li>
         <li class="relative">
@@ -57,7 +57,7 @@
                 />
               </svg>
             </span>
-            <span>Category 1</span>
+            <span>Phase 1</span>
             <span
               class="absolute right-0 ml-auto mr-[0.8rem] transition-transform duration-300 ease-linear motion-reduce:transition-none [&>svg]:text-gray-600 dark:[&>svg]:text-gray-300"
               data-te-sidenav-rotate-icon-ref
@@ -83,9 +83,10 @@
           >
             <li class="relative">
               <a
+                href="/phaseOne"
                 class="flex h-6 cursor-pointer items-center truncate rounded-[5px] py-4 pl-[3.4rem] pr-6 text-[0.78rem] text-gray-600 outline-none transition duration-300 ease-linear hover:bg-slate-50 hover:text-inherit hover:outline-none focus:bg-slate-50 focus:text-inherit focus:outline-none active:bg-slate-50 active:text-inherit active:outline-none data-[te-sidenav-state-active]:text-inherit data-[te-sidenav-state-focus]:outline-none motion-reduce:transition-none dark:text-gray-300 dark:hover:bg-white/10 dark:focus:bg-white/10 dark:active:bg-white/10"
                 data-te-sidenav-link-ref
-                >Link 2</a
+                >Table</a
               >
             </li>
             <li class="relative">
