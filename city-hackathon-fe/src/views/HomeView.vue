a0153f2f42108afd419a06077d422379b7a7712d
brunomaria71 <brunomaria71@yahoo.com>
2023-09-30 18:39:25 -0400

    adding rectangle

--- city-hackathon-fe/src/views/HomeView.vue
+++ city-hackathon-fe/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import Rectangle from '../components/Rectangle.vue'
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <Rectangle />
   </main>
 </template>
